26049ca509bcad58fc95ad1bfc4b23a4d96a1eb7
daniel8610 <daniel1086@hotmail.it>
2015-06-05 15:56:37 +0200

    boilerpipe

--- DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/DataJungleNER.java
+++ DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/DataJungleNER.java
@@ -1,10 +1,19 @@
 package DataJungleNER.DataJungleNER;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
 public class DataJungleNER 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws MalformedURLException, BoilerpipeProcessingException
     {
      String s="cao";
      System.out.println("ultima prova di conflitto");
+     URL url = new URL("http://www.10news.com/newsy/apple-is-late-to-vr-and-ar-but-its-acquisitions-could-help");
+     String text = ArticleExtractor.INSTANCE.getText(url);
+     System.out.println(text);
     }
 }
