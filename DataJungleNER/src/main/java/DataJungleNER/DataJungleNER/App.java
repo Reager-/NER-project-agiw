18f61a8fcff27df98ac3c809cfcc6b5a1ede4589
Reager- <reager.tug@gmail.com>
2015-06-05 15:07:23 +0200

    initial commit

---
+++ DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/App.java
@@ -0,0 +1,13 @@
+package DataJungleNER.DataJungleNER;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
