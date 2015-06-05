c4a6103ca6ee5b2cec959c8add68141af441450c
daniel8610 <daniel1086@hotmail.it>
2015-06-05 15:33:55 +0200

    mod nome

---
+++ DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/DataJungleNER.java
@@ -0,0 +1,13 @@
+package DataJungleNER.DataJungleNER;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataJungleNER 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Merialdo" );
+    }
+}
