a5cf71a83d8a2aa2f6d9d3d618688f381a6ff70b
Reager- <reager.tug@gmail.com>
2015-06-12 16:47:25 +0200

    fixed StanfordNLP

--- DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/MainClass.java
+++ DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/MainClass.java
@@ -1,41 +1,12 @@
 package DataJungleNER.DataJungleNER;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.ListIterator;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-
-
-
-
 public class MainClass {
 	public static void main(String [] args){
 		PropertiesManager p=new PropertiesManager();
-	//NERexecutor e=new NERexecutor(p.getValueForKey("urls"),p.getValueForKey("outputEnt"));
-	//e.exec();
+		NERexecutor e=new NERexecutor(p.getValueForKey("urls"),p.getValueForKey("outputEnt"));
+		e.exec();
 		//NERexecutorSeriale e=new NERexecutorSeriale("target/prova.txt","target/entitàSeriale.txt");
-		//e.exec();
-		
-		
-		WrapperStanfordNLP w=new WrapperStanfordNLP();
-		String s=w.getEntitiesss("We'll be  stuck waiting for the end of the year to see if this happens since California sees most of its annual rainfall during the winter months.In the meantime, many in California are hoping and praying this El Niño event remains strong through next winter, and if forecasts are any indication, there's an 85 percent chance those hopes and prayers will get answered.");
-		System.out.println(s);
-		
-		
-		
-		
-		
-		String[] d=Jsoup.parse(s).getElementsByTag("ORGANIZATION").html().split("/n");
-		LinkedList<String> result = new LinkedList<String>();
-		result.addAll(Arrays.asList(d));
-		for(String z:result){
-			System.out.println(z.equals(" "));
-		}
-		
+		//e.exec();		
 		 
 	}
 
--- DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/WrapperStanfordNLP.java
+++ DataJungleNER/src/main/java/DataJungleNER/DataJungleNER/WrapperStanfordNLP.java
@@ -1,24 +1,30 @@
 package DataJungleNER.DataJungleNER;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.NERClassifierCombiner;
 import edu.stanford.nlp.ie.crf.*;
 import edu.stanford.nlp.ling.CoreLabel;
 
 public class WrapperStanfordNLP implements INamedEntityRecognition{
 	
 	private String serializedClassifier;
-	private AbstractSequenceClassifier<CoreLabel> classifier;
+	//private AbstractSequenceClassifier<CoreLabel> classifier;
+	private NERClassifierCombiner classifier;
 	
 	public WrapperStanfordNLP(){
 		
 		try {
 			serializedClassifier = "training/english.all.3class.nodistsim.crf.ser.gz";
-			classifier = CRFClassifier.getClassifier(serializedClassifier);
+			//classifier = CRFClassifier.getClassifier(serializedClassifier);
+			classifier = new NERClassifierCombiner(false, false, serializedClassifier);
 		} catch (Exception e) {
 			System.out.println("Exception in WrapperStanfordNLP constructor");
 		}
@@ -26,37 +32,41 @@ public class WrapperStanfordNLP implements INamedEntityRecognition{
     
 
     private	LinkedList<String> getAll(String output){
-    	LinkedList<String> l = new LinkedList<String>();
-    	l.add("LOCATION");
-    	String[] r1=Jsoup.parse(output).getElementsByTag("LOCATION").html().split("/n");
-    	if(!r1[0].equals(""))
-    		l.addAll(Arrays.asList(r1));
-    	l.add("ORGANIZATION");
-    	String[] r2=Jsoup.parse(output).getElementsByTag("ORGANIZATION").html().split("/n");
-    	if(!r2[0].equals(""))
-    		l.addAll(Arrays.asList(r2));
-    	l.add("PERSON");
-    	String[] r3=Jsoup.parse(output).getElementsByTag("PERSON").html().split("/n");
-    	if(!r3[0].equals(""))
-    		l.addAll(Arrays.asList(r3));
+    	LinkedList<String> result = new LinkedList<String>();
+    	result.add("ORGANIZATION");
+    	Iterator<Element> it = Jsoup.parse(output).getElementsByTag("ORGANIZATION").iterator();
+    	while (it.hasNext()){
+    		String entity = it.next().text();
+    		if (!result.contains(entity)){
+    			result.add(entity);
+    		}
+    	}
+    	result.add("PERSON");
+    	Iterator<Element> it2 = Jsoup.parse(output).getElementsByTag("PERSON").iterator();
+    	while (it2.hasNext()){
+    		String entity = it2.next().text();
+    		if (!result.contains(entity)){
+    			result.add(entity);
+    		}
+    	}
+    	result.add("LOCATION");
+    	Iterator<Element> it3 = Jsoup.parse(output).getElementsByTag("LOCATION").iterator();
+    	while (it3.hasNext()){
+    		String entity = it3.next().text();
+    		if (!result.contains(entity)){
+    			result.add(entity);
+    		}
+    	}
     	
-    	return l;
+    	return result;
     	
     }
     
 	public LinkedList<String> getEntities(String html) {
 		
 	
-		String output=this.classifier.classifyToString(html, "inlineXML", true); 
-		
+		String output=this.classifier.classifyToString(html, "inlineXML", true);
 		return this.getAll(output);
-	}
-	//da cancellare
-public String getEntitiesss(String html) {
-		
-		
-		 //http://stackoverflow.com/questions/28975890/stanford-ner-running
-		return this.classifier.classifyToString(html, "inlineXML", true);
 	}
 	
 }
